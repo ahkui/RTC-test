cc0739d404e22edada7ce34c2ea317c42c1a9308
KUI\ahkui <ahkui@outlook.com>
2017-08-19 01:10:07 +0800

    up

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('deploy', function()
 {
-    $cmd = 'cd /var/www;ssh-add /root/.ssh/id_rsa;/usr/bin/git fetch 2>&1;/usr/bin/git reset --hard HEAD 2>&1;composer install 2>&1;composer update 2>&1';
+    $cmd = 'cd /var/www;ssh-add /root/.ssh/id_rsa;/usr/bin/git fetch 2>&1;/usr/bin/git reset --hard HEAD 2>&1';
     exec($cmd, $output, $return);
     if ($return !== 0) {
         return response($output,500);
