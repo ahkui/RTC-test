203c107c869b4b311638ff3034e84d14f3f4d128
AhKui <ahkui@outlook.com>
2017-08-18 14:42:35 +0000

    install weebhook

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+Route::post('deploy', function()
+{
+    $deployer = new \Tmd\AutoGitPull\Deployer(array(
+        'directory' => '/var/www/mysite/'
+    ));
+    $deployer->deploy();
+});
+
