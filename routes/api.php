f04caed68dcb8307448d961856a082d32795cf25
joonkui95 <joonkui95@users.noreply.github.com>
2017-09-02 03:56:07 -0500

    add composer install to deploy (#5)

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('deploy', function()
-{
-    ini_set('max_execution_time', 300);
-    $cmd = 'cd /var/www;composer install;php artisan --version';
-    exec($cmd, $output, $return);
-    if ($return !== 0) {
-        return response($output,500);
-        // abort(500,$output);
-    }
-    return $output;
-});
+// Route::get('deploy', function()
+// {
+//     ini_set('max_execution_time', 300);
+//     $cmd = 'cd /var/www;composer dump-autoload -o;composer install;php artisan --version';
+//     exec($cmd, $output, $return);
+//     if ($return !== 0) {
+//         return response($output,500);
+//         // abort(500,$output);
+//     }
+//     return $output;
+// });
 
 // Route::post('composer', function()
 // {
@@ -41,19 +41,12 @@ Route::get('deploy', function()
 //     return $output;
 // });
 
-
-
-
-
 Route::post('deploy', function()
 {
     ini_set('max_execution_time', 300);
-    $cmd = 'cd /var/www;ssh-add /root/.ssh/id_rsa;/usr/bin/git fetch origin 2>&1;/usr/bin/git reset --hard origin/server 2>&1';
+    $cmd = 'cd /var/www;ssh-add /root/.ssh/id_rsa;/usr/bin/git fetch origin 2>&1;/usr/bin/git reset --hard origin/server 2>&1;chmod -R 777 /var/www/storage;composer dump-autoload -o;composer install;php artisan clear-compiled;php artisan view:clear;php artisan config:clear;php artisan optimize;composer dump-autoload -o;php artisan queue:restart';
     exec($cmd, $output, $return);
-    if ($return !== 0) {
-        return response($output,500);
-        // abort(500,$output);
-    }
+    if ($return !== 0) return response($output,500);
     return $output;
 });
 
